d25434973f024ea3b6be141ade3c74039e5656cb
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2024-02-24 14:40:05 +0000

    basic form working

--- current/api/main.py
+++ current/api/main.py
@@ -1,23 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
 from .models import CaseRequest
 from gcsa.event import Event
 from datetime import datetime, timedelta
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-from current.cal.main import calendar
+# from current.cal.main import calendar
 
 app = FastAPI()
 
-@app.post("/caserequest")
-async def create_case_request(case: CaseRequest):
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="current/static/templates")
+@app.get("/", response_class=HTMLResponse)
+async def request_form(request: Request):
+    return templates.TemplateResponse(
+        "request.html", {"request": request, "result": "res"}
+    )
 
+#handle the form, we are expecting it to use the CaseRequest model
+@app.post("/case-request")
+async def create_case_request(casename: str = Form(...), length: int = Form(...), nhsno: int = Form(...)):
+ 
     event = Event(
-        case.operation_name,
+        casename,
         start=datetime.now(),
-        end=datetime.now() + timedelta(minutes=case.estimated_length),
-        description=case.requested_date
+        end=datetime.now() + timedelta(minutes=length),
+        description=nhsno
         )
 
-    calendar.add_event(event)
+    # calendar.add_event(event)
 
-    return case
+    return {"casename": casename, "length": length, "nhsno": nhsno}
+
+# @app.post("/caserequest")
+# async def create_case_request(case: CaseRequest):
+
+#     event = Event(
+#         case.operation_name,
+#         start=datetime.now(),
+#         end=datetime.now() + timedelta(minutes=case.estimated_length),
+#         description=case.requested_date
+#         )
+
+#     calendar.add_event(event)
+
+#     return case
 
--- current/cal/main.py
+++ current/cal/main.py
@@ -1,23 +1,24 @@
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
+import os
 
 from datetime import datetime, timedelta
 
-CURRENT_CAL_ID = "3a784af39e091f5fb78f38e1f205f562548f7cba1819c5a4c2c0ef7c10194e4b@group.calendar.google.com"
+# CURRENT_CAL_ID = "3a784af39e091f5fb78f38e1f205f562548f7cba1819c5a4c2c0ef7c10194e4b@group.calendar.google.com"
 
-CREDENTIALS_PATH = "/home/glfharris/src/current/.credentials/credentials.json"
+# CREDENTIALS_PATH = "creds.json"
 
-calendar = GoogleCalendar(CURRENT_CAL_ID,
-        credentials_path=CREDENTIALS_PATH)
+# calendar = GoogleCalendar(CURRENT_CAL_ID,
+#         credentials_path=CREDENTIALS_PATH)
 
-event = Event(
-        "Appendix",
-        start=datetime.now(),
-        end=datetime.now() + timedelta(minutes=90),
-        description="Some text goes here"
-        )
+# event = Event(
+#         "Appendix",
+#         start=datetime.now(),
+#         end=datetime.now() + timedelta(minutes=90),
+#         description="Some text goes here"
+#         )
 
-calendar.add_event(event)
+# calendar.add_event(event)
 
-for event in calendar:
-    print(event)
+# for event in calendar:
+#     print(event)
